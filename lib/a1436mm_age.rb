c7892826360a31fbcd254307f228d28c709f356c
a1436mm <a1436mm@aiit.ac.jp>
2014-11-04 12:14:06 +0000

    Update Function2

--- lib/a1436mm_age.rb
+++ lib/a1436mm_age.rb
@@ -1,6 +1,8 @@
 require "a1436mm_age/version"
 require "date"
 module A1436mmAge
-    d = Date.today.year - age
-    puts "Your Age:" d
+    def self.birth(age) 
+    d = Date.today.year - age.to_i 
+    return d
+    end
 end
